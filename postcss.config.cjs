8ce578eb0a5cec80dc0c843a3e203017fc9e08fc
ShaheerMinhas <minhasshaheer1@gmail.com>
2024-11-11 00:39:23 +0500

    Initial Commit

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},    // Tells PostCSS to process Tailwind CSS
+    autoprefixer: {},   // Adds vendor prefixes for cross-browser compatibility
+  },
+};
